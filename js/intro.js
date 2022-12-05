8972596e4a78ff9558b4eb26fb693a37abe31914
Ryan Spice <spice.ryan@hotmail.com>
2022-12-05 07:31:31 -0500

    latest codebase

--- js/intro.js
+++ js/intro.js
@@ -3,7 +3,7 @@ var Intro = Object.create(null);
 Intro.prototype = {
 	init:function()
 	{
-		
+
 		this.SplashScreen = new Array();
 		this.SplashScreen[0] = new Image();
 		this.SplashScreen[1] = new Image();
@@ -15,7 +15,7 @@ Intro.prototype = {
 		this.StartAlpha = 2;
 		this.Background = this.graphics.load('intro/background');
 		this.begin = this.graphics.load('intro/begin');
-		
+
 		this.ScoreNumber = new Array();
 		this.ScoreNumber[0] = this.graphics.load('interface/score/score_0');
 		this.ScoreNumber[1] = this.graphics.load('interface/score/score_1');
@@ -27,7 +27,7 @@ Intro.prototype = {
 		this.ScoreNumber[7] = this.graphics.load('interface/score/score_7');
 		this.ScoreNumber[8] = this.graphics.load('interface/score/score_8');
 		this.ScoreNumber[9] = this.graphics.load('interface/score/score_9');
-		
+
 		this.xxx=0;
 		this.start = false;
 
@@ -36,7 +36,7 @@ Intro.prototype = {
 		(this.statusicons = this.app.create(StatusIco.prototype)).init();
 		(this.characterselect = this.app.create(Characterselect.prototype)).init();
 		this.continue = false;
-		
+
 		return this;
 	},
 	draw:function()
@@ -44,59 +44,59 @@ Intro.prototype = {
 
 		if (this.StartPhase==0)
 		{
-			
+
 			this.visuals.rect_ext(-this.app.getScaledWidth(),0,this.app.getScaledWidth()*3,this.app.getHeight(),1,1,0,"#000000");
-			
+
 			if ((this.StartAlpha>100))
 				this.visuals.image_ext(this.Background,this.app.getWidth()/2,this.app.getHeight()/2, 1,Math.sin(this.StartAlpha/120),true);
-			
+
 			if (this.continue==false)
 				this.visuals.image_ext(this.SplashScreen[this.StartPhase],this.app.getWidth()/2,300, 1,Math.sin(this.StartAlpha/120),true);
-			
+
 			this.visuals.rect_ext(this.app.getWidth()/2,this.app.getHeight()/2,this.app.getScaledWidth()*2,this.app.getHeight(), 1,Math.sin(this.StartAlpha/60),true,"#FFFFFF");
-		
+
 		}
 		else
 			if (this.continue==false)
 			this.visuals.image_ext(this.SplashScreen[this.StartPhase],this.app.getWidth()/2,300, 1,Math.sin(this.StartAlpha/120),1);
-			
+
 		this.visuals.rect_gradient(this.app.getWidth()/2,this.app.getHeight()/2,this.Background.width,this.app.getHeight(),1,Math.sin(this.startAlpha/120)*0.9,1,"transparent","#5e5fdf");
 		this.visuals.rect_gradient(this.app.getWidth()/2,this.app.getHeight()*0,this.Background.width,this.app.getHeight()*2,1,Math.sin(this.startAlpha/120)*0.9,1,"transparent","#5e5fdf");
-		
+
 		if (this.continue == true)
 		{
-			
+
 			this.characterselect.draw();
-		
+
 		}
 		else
 			if (this.continue==false)
 			this.visuals.image_ext(this.SplashScreen[this.StartPhase],this.app.getWidth()/2,300, 1,Math.sin(this.StartAlpha/120),true);
-			
-			
+
+
 		if (this.StartPhase==0)
 		{
 			this.particles._draw();
-			
+
 			if (this.graphics.getErrors())
 				this.loading.draw();
 			else	{
 				var a = this.app.client.Math.Clamp(Math.cos(this.xxx/25),0,1);
 				if (this.continue==false)
 				this.visuals.image_ext(this.begin,this.app.getWidth()/2,500, 1,a,1);
-				
+
 				this.app.ext.cursor.set('pointer');
-				
+
 				this.statusicons.draw(Math.sin(this.StartAlpha/120)*0.9);
-				
+
 			}
 		}
 			else	{
-			
+
 					this.loading.draw();
 			}
-			
-		
+
+
 		if (this.StartPhase==0)
 			if (Math.sin(this.StartAlpha/60)<0.8)
 				if (this.app.input.released)
@@ -106,33 +106,32 @@ Intro.prototype = {
 
 	},
 	update:function(){
-		
-		for(var i = 0; i<2; i++){
+
 		this.loading.update();
 		this.statusicons.update();
-		
+
 		if (this.continue == true)
 		{
 			this.characterselect.update();
 			this.particles._update();
-		
+
 			return;
 		}
-		
+
 		if (this.StartPhase==0)
 			if (Math.sin(this.StartAlpha/60)<0.8)
 				this.particles._update();
-		
-		
-				
-				
+
+
+
+
 		if ((this.StartPhase!=0)||(this.StartAlpha<180))
 			this.StartAlpha+= Math.round(1 * this.app.getDelta());
-		
+
 		this.xxx+=Math.round(1 * this.app.getDelta());
-		
+
 		this.ContinueAlpha=Math.sin(this.xxx*0.01);
-		
+
 		if ((this.app.input.y>100)&&(this.app.input.y<this.app.getHeight()-100))
 			if (this.app.input.released)
 				if (this.StartPhase!=0)
@@ -143,18 +142,18 @@ Intro.prototype = {
 		if (this.StartAlpha==359)
 			{
 				this.StartAlpha = 1;
-				
+
 				if (this.StartPhase==0)
 					{
 					this.start = true;
 					return true;
 					}
-				
+
 				this.StartPhase--;
 			}
-		}
+
 		return true;
 
 	}
-	
+
 };
--- js/particles.js
+++ js/particles.js
@@ -1,22 +1,22 @@
 "use strict";
 var SB_Particles = new Array();
-var LastX = 0; 
+var LastX = 0;
 var LastY = 0;
 var SB_ = Object.create(null);
-	SB_.size = 125;
+	SB_.size = 100;//25;
 	SB_.prototype = {
 		_draw:function(){
-		
+
 			for (var _R=0; _R<SB_.size;++_R)
-				SB_Particles[_R].draw(this.visuals);	
-			
+				SB_Particles[_R].draw(this.visuals);
+
 		},
 		_update:function()
 		{
-		
+
 			for (var _R=0; _R<SB_.size;++_R)
 				SB_Particles[_R].update();
-			
+
 		},
 		init:function()
 		{
@@ -38,17 +38,17 @@ var SB_ = Object.create(null);
 
 						SB_Particles[_R].delay = Math.random()*25+15+Math.random()*200*Math.round(Math.random()*2);
 			}
-		
+
 		},
 		update:function()
 		{
 			var w = Application.getWidth();
 			var h = Application.getHeight();
-			
+
 			//if (!Application.getCurrent().intro.statusicons.toggleParticles)
 			if (!Application.toggleParticles)
 				return;
-			
+
 			if(this.delay<1)
 			{
 				this.x-=(window.innerWidth/2-Application.input.x)*0.001;
@@ -71,7 +71,7 @@ var SB_ = Object.create(null);
 			var y = this.y;
 
 			v.linestart();
-		
+
 			for(var s=0;s<this.n;++s)
 				{
 					var cos = Math.cos(s+this.a);
@@ -89,18 +89,18 @@ var SB_ = Object.create(null);
 			v.line(x,y,LastX,LastY,"FFFFFF",1);
 			LastX = x;
 			LastY = y;
-			}	
+			}
 	};
 
 
 
-var part_boardSnow = 
+var part_boardSnow =
 {
 	create:function(x,y,angle)
 	{
 		this.yy = -Math.random()*3;
 		this.y = (y - (30*this.app.getScale()) - (Math.random()*40*this.app.getScale()));
-		
+
 		this.x = x + Math.random()*4- Math.random()*4;
 	//	this.y = y;
 		this.velx = 0;
@@ -108,32 +108,32 @@ var part_boardSnow =
 		this.dir=-1+Math.random()*1;
 		this.t = Math.round(1+Math.random()*9);
 		this.tt = 500-Math.random()*100;
-		
+
 		//this.tt = 25+Math.random()*5;
-		
+
 		this.del = false;
-		
+
 		this.angle = angle;
 		this.alpha = 0.5+angle/15;
 		this.image = this.graphics.load('effects/snow/sc');
-				this.image2 = this.graphics.load('effects/snow/effect_snow_1');		
+				this.image2 = this.graphics.load('effects/snow/effect_snow_1');
 		this.restore = true;
-		
-		
-		
-		
-		
+
+
+
+
+
 		this.x+=1*this.angle*this.app.getScale();
 		this.y-=(this.angle*this.angle);
 		this.fuzz = false;
 		this.scale = 0.2;
-		
+
 			//this.yy-=this.app.getCurrent().game.player.speed/2;
 			//this.yy=-this.app.getCurrent().game.player.speed*1.5;
 		if (Math.sqrt((this.angle*this.angle)>-0.1+Math.random()*1.4))
 			{
 			//this.fuzz = true;
-				
+
 				var ta = 0;
 				if ((this.angle>-0.1)&&(this.angle<0.1))
 					ta = (1 - Math.round(Math.random()*1)) + this.angle*55;
@@ -142,14 +142,14 @@ var part_boardSnow =
 			this.y +=+Math.random()*10;
 			this.scale += 0.1 + Math.random()*1;
 				this.alpha = 1.01;
-		//		
+		//
 			this.yy = -Math.random()*5;
 		}
 	},
 	draw:function(){
-		
-			//this.visuals.rect_centered(this.x+MapOffX,this.y,25,25,"#FFFFFF");	
-			//this.visuals.rect_ext(this.x+MapOffX,this.y+MapOffY,25,35,1,1,1,"#FFFFFF");	
+
+			//this.visuals.rect_centered(this.x+MapOffX,this.y,25,25,"#FFFFFF");
+			//this.visuals.rect_ext(this.x+MapOffX,this.y+MapOffY,25,35,1,1,1,"#FFFFFF");
 		var a = 0.75+Math.sqrt((this.angle*this.angle))*0.15;
 		//a = 1;
 		var x = (this.x+MapOffX);
@@ -159,20 +159,20 @@ var part_boardSnow =
 		//x =  ((MapOffX*this.app.getCurrent().game.mapscale)/2 - (-this.x*0.9*this.app.getScale()));
 		//x =  ((this.x*this.app.getCurrent().game.mapscale)/2 - (-MapOffX*0.9*this.app.getScale()));
 				//this.visuals.image_rotate(this.image,this.x+MapOffX,this.y+MapOffY+this.image.height/2.8,1,0.75*-this.angle*57.2957795,a*2,-this.image.width/2,-this.image.height/2);
-			
+
 		if (this.fuzz)
 			this.visuals.image_rotate(this.image2,this.x+MapOffX,y+this.image.height/2,a+this.scale,110,a/2,-this.image.width/2,-this.image.height/2);
 			else
 				this.visuals.image_rotate(this.image,x,y+this.image.height/2,this.scale,0.75*-this.angle*57.2957795,a*this.alpha,-this.image.width/2,-this.image.height/2);
-			
-		
-		
+
+
+
 	},
 	update:function()
 	{
 		if (this.scale<1)
 			this.scale+=0.01;
-		
+
 		var MapSpeed = 1;
 		if (this.del){
 			//this.x = ((this.x)/2 - (-MapOffX*this.app.getScale()));
@@ -181,43 +181,43 @@ var part_boardSnow =
 			this.x = this.app.getCurrent().game.player.x+MapOffX	;
 
 			this.create(this.x,this.y,this.app.getCurrent().game.player.angle);
-			
+
 		}
 		this.t-=1;
 		this.tt-=this.t*this.app.getDelta();
 		//if (this.t<0.01)
 			//this.t =  Math.round(1+Math.random()*9);
-			
+
 		if (this.tt<0)
 			this.del = true;
-		
+
 		this.yy = this.app.client.Math.Clamp(this.yy,0,0);
 		this.tt-=this.t*this.app.getDelta();
 		this.t-=0.01;
 		if (this.t<0.1)
 			this.t =  Math.round(1+Math.random()*9);
-			
+
 		if (this.tt<0)
 			this.del = true;
-		
+
 		if (this.fuzz){
-			
+
 			this.y+=this.yy;
-			
-			
+
+
 			//if (this.angle>0.8)
 			//	this.velx+=1.1;
 			//	else
 			//if (this.angle<0.8)
 			//	this.velx-=1.1;
 		}
-		
+
 		var MapSpeed = 1;
 		//if (this.yy<0)
-		//this.yy+=0.1;		
+		//this.yy+=0.1;
 		this.vely = -this.app.getCurrent().game.player.speed;
-		
-		
+
+
 		//App.visuals.image_rotate2(this.image,MapOffX+this.x,this.y+MapOffY+this.image.height/1.8,1,0.5+this.angle,-this.image.width/2,-this.image.height/2,1);
 		this.y-=this.yy;
 		this.yy*=0.9;
@@ -225,10 +225,10 @@ var part_boardSnow =
 		return;
 	},
 	init:function(x,y,angle){
-		
-		
+
+
 		this.create(x,y,angle);
-		
+
 		return this;
 	}
 }
